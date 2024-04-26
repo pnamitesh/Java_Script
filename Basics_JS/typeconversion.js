78f936e2254bf484905cc89141578e5912f71d4b
amitesh <pandeyamitesh2812@gmail.com>
2024-04-26 23:07:12 +0530

    operations

--- Basics_JS/typeconversion.js
+++ Basics_JS/typeconversion.js
@@ -19,3 +19,21 @@ let isloggedin=1
 let booleanisloggedin= Boolean(isloggedin)// converted to true 
 console.log(booleanisloggedin);
 
+//********************* Operations *********************
+
+// let value =3
+// let negvalue= -value
+// console.log(negvalue);
+// console.log(2**8);
+
+let str1= "hey"
+let str2="amit"
+
+let str3=str1+str2
+console.log(str3);
+
+console.log("1"+2);// op 12
+console.log(1+"2");// op 12
+console.log("1"+2+2);//122
+console.log(2+2+"1");//41     string baad main likhi hai isliye 
+console.log(+true);//1
