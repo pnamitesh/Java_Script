26aae74802771caa94bb3473f8603b930471c726
amitesh <pandeyamitesh2812@gmail.com>
2024-04-26 23:15:36 +0530

    comparision

---
+++ Basics_JS/comparisions.js
@@ -0,0 +1,16 @@
+console.log(2>1)
+console.log(2<1)
+console.log(2==1)
+console.log(2!=1)
+
+
+console.log("2">1)// automaticaly JS ne convert kardiya number main
+
+// typescript doesnt allow to comapre different datatype
+
+console.log(null==0)
+console.log(null>=0)// convert kardiya null ko 0 main JS bs yahi prob aati hai ...
+console.log(null<0)
+// === 
+console.log("2"===2) // STRICT cheack ye value ke saath saath datatype ko bhi cheak karta hai 
+console.log("2"==2)
