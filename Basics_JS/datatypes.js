1c17511ab702b425d4828555777fff4a6a1904d8
amitesh <pandeyamitesh2812@gmail.com>
2024-04-26 22:36:34 +0530

    datatypes

---
+++ Basics_JS/datatypes.js
@@ -0,0 +1,20 @@
+// alert(3+3)  we are using node js not Browser.
+
+console.log(3+3) 
+console.log("amitesh")// code should be readable ...
+
+
+let name="amitesh"
+let age=18
+let isloggedin= false
+// primitive 
+//number , bigint , string , boolean  , 
+// null(representatin of empty value ) , 
+// undefined( decale variable and still values is not assigned ) ,
+// symbol  (unique) 
+
+// non primitive
+// Object ,array , function
+
+console.log(typeof null);// output --> Object
+console.log(typeof undefined);// output --> undefined
