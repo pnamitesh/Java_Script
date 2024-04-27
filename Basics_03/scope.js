d2beb7d0ba87169bb9bd9b0de2b4c6d8e7531a6f
amitesh <pandeyamitesh2812@gmail.com>
2024-04-27 19:34:58 +0530

    scope & hoisting

--- Basics_03/scope.js
+++ Basics_03/scope.js
@@ -4,7 +4,45 @@
 // var ki value block ke bahr bhii available hoti hai 
 //  local and global scope are there
 
+const user = {
+    username: "hitesh",
+    price: 999,
 
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+
+
+const chai =  () => {
+    let username = "hitesh"
+    console.log(this);
+}
+
+
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addTwo(3, 4))
+// Basically yahan nested functions ka use karke ye bataya hain ki....
+// child function parent function ka variable access kar sakta hain but parent nahi ...or 
+// variable ek fn bhi store kar sakta hain pr ismain fn ko upr se call nahi karsakte see eg.. 
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
 
 
 
