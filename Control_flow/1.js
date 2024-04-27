6cb842a0f33624660ac86db41550b4257f097b4e
amitesh <pandeyamitesh2812@gmail.com>
2024-04-28 00:15:13 +0530

    basics abt if -else

---
+++ Control_flow/1.js
@@ -0,0 +1,33 @@
+// if - else - else if statement : var keyword ka scope hai global
+// <,>,<=,>=,!=,==,==,!==
+// &&,||,!.
+
+//switch (ek value ko multiple conditions se cheack karne ke liye switch ka use karte hain)
+
+// switch (key) {              // syntax of switch case statement .....
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+//truthy and falsy values
+
+// falsy values   ; false ,0,-0,Bigint 0n,"", null,undefined , NaN.
+
+// truthy values  ; "0",'false'," ",[],{},function(){}.
+
+// Nullish coalescing operator ??: null undefined 
+
+let val1;
+// val1=5 ?? 10
+
+val1 =null??5
+val1 =undefined??15
+
+console.log(val1);
+
+
